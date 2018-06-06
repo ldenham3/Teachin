4208ce5d882bb4b481f569ebbb8d9b4a675dc0a4
Leah Denham <ldenham@fitbit.com>
2018-06-05 19:47:00 -0700

    initial commit

---
+++ calculator.py
@@ -0,0 +1,18 @@
+#generally prefer class over static methods
+#CLS represents whole class, allows it to see methods
+#Why would you do a static method? adds context, we know its related to the class
+
+
+class Calculator(object):
+	def __init__(self):
+		pass
+
+	@staticmethod
+	def add(num1 + num2):
+		return num1 + num2
+
+	@classmethod
+	def subtract(cls, num1, num2):
+		return cls.add(num1, num2 * -1)
+
+print(Calculator.add(1,2))
